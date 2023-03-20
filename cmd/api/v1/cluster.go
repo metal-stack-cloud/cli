f04fd8f8dc742bce0f9dbe44fc74d1920c2d3d93
simcod <mailto.smayer@googlemail.com>
2023-03-20 11:34:02 +0100

    cluster update command with / without file option

--- cmd/api/v1/cluster.go
+++ cmd/api/v1/cluster.go
@@ -55,19 +55,24 @@ func NewClusterCmd(c *config.Config) *cobra.Command {
 		DeleteCmdMutateFn: func(cmd *cobra.Command) {
 			cmd.Flags().StringP("project", "", "", "project of the cluster")
 		},
+		UpdateCmdMutateFn: func(cmd *cobra.Command) {
+			cmd.Flags().StringP("uuid", "", "", "uuid of the cluster")
+			cmd.Flags().StringP("project", "", "", "project of the cluster")
+			cmd.Flags().StringP("kubernetes", "", "", "kubernetes version of the cluster")
+			// cmd.Flags().StringP("workername", "", "", "name of the worker running the cluster")
+			// cmd.Flags().StringP("machinetype", "", "", "machine type of the worker")
+			// cmd.Flags().StringP("minsize", "", "", "minimal workers of the cluster")
+			// cmd.Flags().StringP("maxsize", "", "", "maximal workers of the cluster")
+			// cmd.Flags().StringP("maxsurge", "", "", "maximal surge of the clusters")
+			// cmd.Flags().StringP("maxunavailable", "", "", "maximal unavailable workers of the cluster")
+			cmd.Flags().StringP("maintenancebegin", "", "", "time for a possible maintenance begin for the worker")
+			cmd.Flags().StringP("maintenanceduration", "", "", "duration of a possible maintenance for the worker")
+		},
 		CreateRequestFromCLI: func() (*connect.Request[apiv1.ClusterServiceCreateRequest], error) {
 
-			maintenancebegin := viper.GetString("maintenancebegin")
+			parsedMaintenanceBegin := parseMaintenanceBegin(viper.GetString("maintenancebegin"))
 
-			const format = "Jan 2, 2006 at 3:04pm (MST)"
-			t, _ := time.Parse(format, "Jan 1, 1970 at "+maintenancebegin+" (PST)")
-			timeInUnix := t.Unix()
-			timeInUTC := time.Unix(timeInUnix, 0).UTC()
-			ts := timestamppb.New(timeInUTC)
-
-			maintenanceduration := viper.GetString("maintenanceduration")
-			hours, _ := time.ParseDuration(maintenanceduration)
-			duration := durationpb.New(time.Duration(hours))
+			parsedMaintenanceDuration := parseMaintenanceDuration(viper.GetString("maintenanceduration"))
 
 			clustercr := &apiv1.ClusterServiceCreateRequest{
 				Name:      viper.GetString("name"),
@@ -86,15 +91,46 @@ func NewClusterCmd(c *config.Config) *cobra.Command {
 				},
 				Maintenance: &apiv1.Maintenance{
 					TimeWindow: &apiv1.MaintenanceTimeWindow{
-						Begin:    ts,
-						Duration: duration,
+						Begin:    &parsedMaintenanceBegin,
+						Duration: &parsedMaintenanceDuration,
 					},
 				},
 			}
 			return connect.NewRequest(clustercr), nil
 		},
+		UpdateRequestFromCLI: w.updateFromCLI,
 	}
 	return genericcli.NewCmds(cmdsConfig)
+}
+
+func (c *cluster) updateFromCLI(args []string) (*apiv1.Cluster, error) {
+	clusterToUpdate, err := c.Get(viper.GetString("uuid"))
+	if err != nil {
+		return nil, fmt.Errorf("unable to retrieve cluster: %w", err)
+	}
+	if viper.IsSet("project") {
+		clusterToUpdate.Project = viper.GetString("project")
+	}
+	if clusterToUpdate.Project == "" {
+		return nil, fmt.Errorf("project must be provided")
+	}
+	if viper.IsSet("kubernetes") {
+		clusterToUpdate.Kubernetes.Version = viper.GetString("kubernetes")
+	}
+	//TODO Should it be possible to update workers too?
+	// if viper.IsSet("workername") {
+	// 	clusterToUpdate.GetWorkers()
+	// }
+	if viper.IsSet("maintenancebegin") {
+		parsedMaintenance := parseMaintenanceBegin(viper.GetString("maintenancebegin"))
+		clusterToUpdate.Maintenance.TimeWindow.Begin = &parsedMaintenance
+		fmt.Println(clusterToUpdate)
+	}
+	if viper.IsSet("maintenanceduration") {
+		parsedDuration := parseMaintenanceDuration(viper.GetString("maintenanceduration"))
+		clusterToUpdate.Maintenance.TimeWindow.Duration = &parsedDuration
+	}
+	return clusterToUpdate, nil
 }
 
 func (c *cluster) Create(rq *connect.Request[apiv1.ClusterServiceCreateRequest]) (*apiv1.Cluster, error) {
@@ -157,12 +193,33 @@ func (c *cluster) List() ([]*apiv1.Cluster, error) {
 
 // TODO implement update command and write tests
 func (c *cluster) Update(rq *apiv1.Cluster) (*apiv1.Cluster, error) {
+	// var workerUpdate []*apiv1.WorkerUpdate
+	// for _, worker := range rq.Workers {
+	// 	workerUpdate = append(workerUpdate, &apiv1.WorkerUpdate{
+	// 		Name:           worker.Name,
+	// 		MachineType:    &worker.MachineType,
+	// 		Minsize:        &worker.Minsize,
+	// 		Maxsize:        &worker.Maxsize,
+	// 		Maxsurge:       &worker.Maxsurge,
+	// 		Maxunavailable: &worker.Maxunavailable,
+	// 	})
+	// }
+	// TODO I guess the update of the maintenance field is not working right now.
 	resp, err := c.c.Apiv1Client.Cluster().Update(c.c.Ctx, &connect.Request[apiv1.ClusterServiceUpdateRequest]{
 		Msg: &apiv1.ClusterServiceUpdateRequest{
 			Uuid:       rq.Uuid,
 			Project:    rq.Project,
 			Kubernetes: rq.Kubernetes,
-			Workers:    []*apiv1.WorkerUpdate{},
+			Maintenance: &apiv1.Maintenance{
+				TimeWindow: &apiv1.MaintenanceTimeWindow{
+					Begin: &timestamppb.Timestamp{
+						Seconds: rq.Maintenance.TimeWindow.Begin.Seconds,
+					},
+					Duration: &durationpb.Duration{
+						Seconds: rq.Maintenance.TimeWindow.Duration.Seconds,
+					},
+				},
+			},
 		},
 	})
 	if err != nil {
@@ -176,7 +233,7 @@ func (c *cluster) ToCreate(r *apiv1.Cluster) (*connect.Request[apiv1.ClusterServ
 }
 
 func (c *cluster) ToUpdate(r *apiv1.Cluster) (*apiv1.Cluster, error) {
-	return nil, nil
+	return clusterResponseToUpdate(r), nil
 }
 
 func clusterResponseToCreate(r *apiv1.Cluster) *connect.Request[apiv1.ClusterServiceCreateRequest] {
@@ -190,4 +247,23 @@ func clusterResponseToCreate(r *apiv1.Cluster) *connect.Request[apiv1.ClusterSer
 			Maintenance: r.Maintenance,
 		},
 	}
+}
+
+func clusterResponseToUpdate(r *apiv1.Cluster) *apiv1.Cluster {
+	return r
+}
+
+func parseMaintenanceBegin(m string) timestamppb.Timestamp {
+	const format = "Jan 2, 2006 at 3:04pm (MST)"
+	t, _ := time.Parse(format, "Jan 1, 1970 at "+m+" (PST)")
+	timeInUnix := t.Unix()
+	timeInUTC := time.Unix(timeInUnix, 0).UTC()
+	ts := timestamppb.New(timeInUTC)
+	return *ts
+}
+
+func parseMaintenanceDuration(m string) durationpb.Duration {
+	hours, _ := time.ParseDuration(m)
+	duration := durationpb.New(time.Duration(hours))
+	return *duration
 }
