0d30bb94a3f717b7b1519f809057ebff1dc97715
Stefan Majer <stefan.majer@f-i-ts.de>
2023-07-01 10:29:37 +0200

    add plugin support, use updated API

---
+++ cmd/api/v1/api.go
@@ -0,0 +1,17 @@
+package v1
+
+import (
+	"github.com/metal-stack-cloud/cli/cmd/config"
+	"github.com/spf13/cobra"
+)
+
+func AddCmds(cmd *cobra.Command, c *config.Config) {
+
+	cmd.AddCommand(newVersionCmd(c))
+	cmd.AddCommand(newHealthCmd(c))
+	cmd.AddCommand(newAssetCmd(c))
+	cmd.AddCommand(newTokenCmd(c))
+	cmd.AddCommand(newIPCmd(c))
+	cmd.AddCommand(newStorageCmd(c))
+
+}
