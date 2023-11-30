3cc978b39fd030b188a72dbcb450fef57c75b2d3
Gerrit <info@gerritschwerthelm.de>
2023-11-30 10:52:08 +0100

    Also allow sorting by description.

--- cmd/sorters/token.go
+++ cmd/sorters/token.go
@@ -13,6 +13,9 @@ func TokenSorter() *multisort.Sorter[*apiv1.Token] {
 		"user": func(a, b *apiv1.Token, descending bool) multisort.CompareResult {
 			return multisort.Compare(a.UserId, b.UserId, descending)
 		},
+		"description": func(a, b *apiv1.Token, descending bool) multisort.CompareResult {
+			return multisort.Compare(a.Description, b.Description, descending)
+		},
 		"expires": func(a, b *apiv1.Token, descending bool) multisort.CompareResult {
 			return multisort.Compare(a.Expires.AsTime().UnixMilli(), b.Expires.AsTime().UnixMilli(), descending)
 		},
