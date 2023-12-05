30120e87cb9d0e39efe3356e205da6b5f1d7f991
Gerrit <Gerrit91@users.noreply.github.com>
2023-12-05 09:21:20 +0100

    Improve admin token list. (#25)

--- cmd/sorters/token.go
+++ cmd/sorters/token.go
@@ -10,8 +10,14 @@ func TokenSorter() *multisort.Sorter[*apiv1.Token] {
 		"id": func(a, b *apiv1.Token, descending bool) multisort.CompareResult {
 			return multisort.Compare(a.Uuid, b.Uuid, descending)
 		},
+		"user": func(a, b *apiv1.Token, descending bool) multisort.CompareResult {
+			return multisort.Compare(a.UserId, b.UserId, descending)
+		},
+		"description": func(a, b *apiv1.Token, descending bool) multisort.CompareResult {
+			return multisort.Compare(a.Description, b.Description, descending)
+		},
 		"expires": func(a, b *apiv1.Token, descending bool) multisort.CompareResult {
 			return multisort.Compare(a.Expires.AsTime().UnixMilli(), b.Expires.AsTime().UnixMilli(), descending)
 		},
-	}, multisort.Keys{{ID: "id"}})
+	}, multisort.Keys{{ID: "user"}, {ID: "expires"}, {ID: "id"}})
 }
