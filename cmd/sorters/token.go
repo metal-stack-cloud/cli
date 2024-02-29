a81eb7b2985877b1dffd00366b4bdea0bfd52856
Gerrit <Gerrit91@users.noreply.github.com>
2024-02-29 12:54:42 +0100

    Show token types in table printer. (#47)

--- cmd/sorters/token.go
+++ cmd/sorters/token.go
@@ -13,11 +13,14 @@ func TokenSorter() *multisort.Sorter[*apiv1.Token] {
 		"user": func(a, b *apiv1.Token, descending bool) multisort.CompareResult {
 			return multisort.Compare(a.UserId, b.UserId, descending)
 		},
+		"type": func(a, b *apiv1.Token, descending bool) multisort.CompareResult {
+			return multisort.Compare(a.TokenType.String(), b.TokenType.String(), descending)
+		},
 		"description": func(a, b *apiv1.Token, descending bool) multisort.CompareResult {
 			return multisort.Compare(a.Description, b.Description, descending)
 		},
 		"expires": func(a, b *apiv1.Token, descending bool) multisort.CompareResult {
 			return multisort.Compare(a.Expires.AsTime().UnixMilli(), b.Expires.AsTime().UnixMilli(), descending)
 		},
-	}, multisort.Keys{{ID: "user"}, {ID: "expires"}, {ID: "id"}})
+	}, multisort.Keys{{ID: "type"}, {ID: "user"}, {ID: "expires"}, {ID: "id"}})
 }
