21b869763836b37b1480c2c75c8e4f72900281a1
Gerrit <info@gerritschwerthelm.de>
2024-07-18 13:42:20 +0200

    Required flag.

--- cmd/admin/v1/tenant.go
+++ cmd/admin/v1/tenant.go
@@ -104,6 +104,7 @@ func newTenantCmd(c *config.Config) *cobra.Command {
 		ValidArgsFunction: c.Completion.AdminTenantListCompletion,
 	}
 
+	genericcli.Must(addBalanceCmd.MarkFlagRequired("euro"))
 	addBalanceCmd.Flags().Uint64P("euro", "", 0, "optional add a balance in cent to the customer balance")
 
 	revokeCmd := &cobra.Command{
