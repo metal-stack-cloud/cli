3003f4bfbd7c81c503dc89c17ab25f103f4f0dc2
Gerrit <Gerrit91@users.noreply.github.com>
2025-10-09 16:36:40 +0200

    Allow retrieving kubeconfigs without exec config, too. (#97)

--- cmd/admin/v1/cluster.go
+++ cmd/admin/v1/cluster.go
@@ -72,8 +72,14 @@ func newClusterCmd(c *config.Config) *cobra.Command {
 	}
 
 	kubeconfigCmd.Flags().DurationP("expiration", "", 8*time.Hour, "kubeconfig will expire after given time")
-	kubeconfigCmd.Flags().Bool("merge", true, "merges the kubeconfig into default kubeconfig instead of printing it to the console")
+	kubeconfigCmd.Flags().Bool("merge", true, "merges the kubeconfig into the current kubeconfig")
+	kubeconfigCmd.Flags().Bool("print-only", false, "only prints the kubeconfig to the console instead of writing it")
+	kubeconfigCmd.Flags().String("auth-type", string(kubernetes.AuthTypeExec), `the way how the resulting kubeconfig authenticates at the api server. can be "exec" or "certs".
+	  "exec" injects an exec config into the kubeconfig, which uses this CLI to automatically renew certificates when they expire.
+	  "certs" simply adds the client certificates to the kubeconfig, there is no automatic renewal once the certificates have expired, the CLI is not called automatically.`)
 	kubeconfigCmd.Flags().String("kubeconfig", "", "specify an explicit path for the merged kubeconfig to be written, defaults to default kubeconfig paths if not provided")
+
+	genericcli.Must(kubeconfigCmd.RegisterFlagCompletionFunc("auth-type", c.Completion.ClusterKubeconfigAuthType))
 
 	// metal admin cluster machine list
 
@@ -195,26 +201,22 @@ func (c *cluster) kubeconfig(args []string) error {
 		return fmt.Errorf("failed to get cluster credentials: %w", err)
 	}
 
-	if !viper.GetBool("merge") {
-		_, _ = fmt.Fprintln(c.c.Out, resp.Msg.Kubeconfig)
-		return nil
-	}
-
-	var (
-		kubeconfigPath = viper.GetString("kubeconfig")
-	)
-
-	merged, err := kubernetes.MergeKubeconfig(c.c.Fs, []byte(resp.Msg.Kubeconfig), pointer.PointerOrNil(kubeconfigPath), nil, c.c.GetProject(), id) // FIXME: reverse lookup project name
+	kubeconfig, err := kubernetes.NewKubeconfigFromRaw(c.c.Fs, c.c.In, c.c.Out, []byte(resp.Msg.Kubeconfig), nil, c.c.GetProject(), id) // FIXME: reverse lookup project name
 	if err != nil {
 		return err
 	}
 
-	err = afero.WriteFile(c.c.Fs, merged.Path, merged.Raw, 0600)
+	if viper.GetBool("print-only") {
+		_, _ = fmt.Fprintln(c.c.Out, string(kubeconfig.Raw))
+		return nil
+	}
+
+	err = afero.WriteFile(c.c.Fs, kubeconfig.Path, kubeconfig.Raw, 0600)
 	if err != nil {
 		return fmt.Errorf("unable to write merged kubeconfig: %w", err)
 	}
 
-	_, _ = fmt.Fprintf(c.c.Out, "%s merged context %q into %s\n", color.GreenString("✔"), merged.ContextName, merged.Path)
+	_, _ = fmt.Fprintf(c.c.Out, "%s wrote context %q into %s\n", color.GreenString("✔"), kubeconfig.ContextName, kubeconfig.Path)
 
 	return nil
 }
---
+++ cmd/kubernetes/kubeconfig.go
@@ -0,0 +1,180 @@
+package kubernetes
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"strings"
+
+	"github.com/fatih/color"
+	"github.com/metal-stack/metal-lib/pkg/genericcli"
+	"github.com/spf13/afero"
+	"github.com/spf13/viper"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/tools/clientcmd/api"
+
+	configlatest "k8s.io/client-go/tools/clientcmd/api/latest"
+	configv1 "k8s.io/client-go/tools/clientcmd/api/v1"
+)
+
+type authType string
+
+const (
+	AuthTypeClientCerts authType = "certs"
+	AuthTypeExec        authType = "exec"
+)
+
+type Kubeconfig struct {
+	Raw         []byte
+	Path        string
+	ContextName string
+}
+
+func NewKubeconfigFromRaw(fs afero.Fs, in io.Reader, out io.Writer, raw []byte, projectName *string, projectid, clusterid string) (*Kubeconfig, error) {
+	path := os.Getenv(clientcmd.RecommendedConfigPathEnvVar)
+	if userPath := viper.GetString("kubeconfig"); userPath != "" {
+		path = userPath
+	}
+	if path == "" {
+		path = clientcmd.RecommendedHomeFile
+	}
+
+	if strings.Contains(path, ":") {
+		return nil, fmt.Errorf("it is currently not supported to merge when multiple kubeconfigs are provided")
+	}
+
+	exists, err := afero.Exists(fs, path)
+	if err != nil {
+		return nil, fmt.Errorf("unable to determine if file exists: %w", err)
+	}
+
+	kubeconfig := &configv1.Config{}
+	err = runtime.DecodeInto(configlatest.Codec, raw, kubeconfig)
+	if err != nil {
+		return nil, fmt.Errorf("unable to decode kubeconfig: %w", err)
+	}
+
+	var (
+		authInfo    *configv1.AuthInfo
+		cluster     *configv1.NamedCluster
+		clusterName string
+	)
+
+	for _, cl := range kubeconfig.Clusters {
+		prefix, _, found := strings.Cut(cl.Name, "-external")
+		if !found {
+			continue
+		}
+		_, clusterName, found = strings.Cut(prefix, "--")
+		if !found {
+			continue
+		}
+
+		cluster = &cl
+	}
+
+	for _, a := range kubeconfig.AuthInfos {
+		if !strings.HasSuffix(a.Name, clusterName+"-external") {
+			continue
+		}
+
+		authInfo = &a.AuthInfo
+	}
+
+	if authInfo == nil || cluster == nil || clusterName == "" {
+		return nil, fmt.Errorf("internal error: kubeconfig does not contain all required information, please update client or raise ticket on metalstack.cloud")
+	}
+
+	contextName := fmt.Sprintf("%s@metalstack.cloud", clusterName)
+	if projectName != nil {
+		contextName = fmt.Sprintf("%s-%s@metalstack.cloud", clusterName, *projectName)
+	}
+
+	currentConfig := &api.Config{
+		Clusters:  map[string]*api.Cluster{},
+		Contexts:  map[string]*api.Context{},
+		AuthInfos: map[string]*api.AuthInfo{},
+	}
+
+	if viper.GetBool("merge") {
+		var err error
+		currentConfig, err = clientcmd.LoadFromFile(path)
+		if err != nil {
+			return nil, fmt.Errorf("error loading kubeconfig: %w", err)
+		}
+	} else {
+		if exists {
+			err = genericcli.PromptCustom(&genericcli.PromptConfig{
+				Message:     fmt.Sprintf(color.YellowString("There is already a file at %s. In combination with --merge=false this file will be overwritten. Are you sure you want to continue?"), path),
+				ShowAnswers: true,
+				In:          in,
+				Out:         out,
+			})
+			if err != nil {
+				return nil, err
+			}
+		}
+	}
+
+	currentConfig.Contexts[contextName] = &api.Context{
+		Cluster:  contextName,
+		AuthInfo: contextName,
+	}
+
+	currentConfig.Clusters[contextName] = &api.Cluster{
+		Server:                   cluster.Cluster.Server,
+		CertificateAuthorityData: cluster.Cluster.CertificateAuthorityData,
+	}
+
+	if currentConfig.CurrentContext == "" {
+		currentConfig.CurrentContext = contextName
+	}
+
+	auth := AuthTypeExec
+	if viper.GetString("auth-type") != "" {
+		auth = authType(viper.GetString("auth-type"))
+	}
+
+	switch auth {
+	case AuthTypeExec:
+		metalcli, err := os.Executable()
+		if err != nil {
+			return nil, fmt.Errorf("unable to get executable path: %w", err)
+		}
+
+		currentConfig.AuthInfos[contextName] = &api.AuthInfo{
+			Exec: &api.ExecConfig{
+				Command:         metalcli,
+				Args:            []string{"cluster", "exec-config", "-p", projectid, clusterid},
+				APIVersion:      "client.authentication.k8s.io/v1",
+				InteractiveMode: api.IfAvailableExecInteractiveMode,
+			},
+		}
+
+		ec, err := NewUserExecCache(fs)
+		if err != nil {
+			return nil, err
+		}
+		// remove cached credentials so a new one will be created
+		_ = ec.Clean(clusterid)
+	case AuthTypeClientCerts:
+		currentConfig.AuthInfos[contextName] = &api.AuthInfo{
+			ClientCertificateData: authInfo.ClientCertificateData,
+			ClientKeyData:         authInfo.ClientKeyData,
+		}
+	default:
+		return nil, fmt.Errorf("unsupported auth type for kubeconfig: %s", auth)
+	}
+
+	merged, err := runtime.Encode(configlatest.Codec, currentConfig)
+	if err != nil {
+		return nil, fmt.Errorf("unable to encode kubeconfig: %w", err)
+	}
+
+	return &Kubeconfig{
+		Raw:         merged,
+		ContextName: contextName,
+		Path:        path,
+	}, nil
+}
